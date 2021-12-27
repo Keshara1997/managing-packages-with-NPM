ca4ec0d7d343668a3778f6b6eb70590ad16106af
Keshara1997 <kavindakeshara97@gmail.com>
2021-12-27 13:29:04 +0530

    Setting up app for basic node and express tutorial new1

--- myApp.js
+++ myApp.js
@@ -6,36 +6,36 @@ var bodyParser = require('body-parser');
 
 // --> 7)  Mount the Logger middleware here
 
-app.use(function(req, res, next) {
-    console.log(req.method + " " + req.path + " - " + req.ip);
-    next();
-});
+// app.use(function(req, res, next) {
+//     console.log(req.method + " " + req.path + " - " + req.ip);
+//     next();
+// });
 
 // --> 11)  Mount the body-parser middleware  here
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
 /** 1) Meet the node console. */
-//bGround.log("Hello World");
-console.log("Hello World");
+bGround.log("Hello World");
+// console.log("Hello World");
 
 /** 2) A first working Express Server */
 
 
 /** 3) Serve an HTML file */
 
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
-});
+// app.get("/", (req, res) => {
+//     res.sendFile(__dirname + "/views/index.html");
+// });
 
 /** 4) Serve static assets  */
 
-app.use(express.static(__dirname + "/public"));
-app.use('/public', express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
+// app.use('/public', express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
 
@@ -47,15 +47,15 @@ app.use('/public', express.static(__dirname + "/public"));
 
 /** 6) Use the .env file to configure the app */
 
-app.get("/json", function(req, res) {
-    var jsonResponse = { "message": "Hello json" };
+// app.get("/json", function(req, res) {
+//     var jsonResponse = { "message": "Hello json" };
 
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        jsonResponse.message = jsonResponse.message.toUpperCase()
-    }
+//     if (process.env.MESSAGE_STYLE === "uppercase") {
+//         jsonResponse.message = jsonResponse.message.toUpperCase()
+//     }
 
-    res.json(jsonResponse);
-});
+//     res.json(jsonResponse);
+// });
 
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
@@ -63,29 +63,29 @@ app.get("/json", function(req, res) {
 
 /** 8) Chaining middleware. A Time server */
 
-function getTheCurrentTimeString() {
-    return new Date().toString();
-}
+// function getTheCurrentTimeString() {
+//     return new Date().toString();
+// }
 
-app.get("/now", (req, res, next) => {
-    req.time = getTheCurrentTimeString();
-    next();
-}, (req, res) => {
-    res.json({ time: req.time });
-});
+// app.get("/now", (req, res, next) => {
+//     req.time = getTheCurrentTimeString();
+//     next();
+// }, (req, res) => {
+//     res.json({ time: req.time });
+// });
 
 /** 9)  Get input from client - Route parameters */
 
-app.get("/:word/echo", (req, res) => {
-    res.json({ echo: req.params.word });
-});
+// app.get("/:word/echo", (req, res) => {
+//     res.json({ echo: req.params.word });
+// });
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
 
-app.get("/name", (req, res) => {
-    res.json({ name: req.query.first + " " + req.query.last });
-});
+// app.get("/name", (req, res) => {
+//     res.json({ name: req.query.first + " " + req.query.last });
+// });
 
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
@@ -93,9 +93,9 @@ app.get("/name", (req, res) => {
 
 /** 12) Get data form POST  */
 
-app.post("/name", function(req, res) {
-    res.json({ name: req.body.first + " " + req.body.last });
-});
+// app.post("/name", function(req, res) {
+//     res.json({ name: req.body.first + " " + req.body.last });
+// });
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
