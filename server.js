a583a6561d0b64edc54e24c2e7d81bad9f4f6f3e
Keshara1997 <kavindakeshara97@gmail.com>
2021-12-29 06:22:02 +0530

    Add aroutr to sent Hellp Express

--- server.js
+++ server.js
@@ -55,6 +55,9 @@
  //      }
  //  })
 
+ app.get("/", function(req, res) {
+     res.send("Hello Express");
+ })
  var port = process.env.PORT || 3000;
  bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function() {
      bGround.log('Node.js listening on port' + port + "...");
