60bf72f179b3d92f8eb8bea377881199292e80ac
Keshara1997 <kavindakeshara97@gmail.com>
2021-12-30 18:13:05 +0530

    Add static middlue ware for assets

--- server.js
+++ server.js
@@ -24,6 +24,9 @@
      });
  }
 
+ app.use(express.static(__dirname + "/public"));
+ app.use('/public', express.static(__dirname + "/public"));
+
  //  app.use('/public', express.static(process.cwd() + '/public'));
 
  //  app.route('/_api/package.json')
